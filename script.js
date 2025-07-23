d84d7a77b5c9ebc1acbf706ce25217d9fd4b5aa4
vivekmahato31 <vivekmahato31@users.noreply.github.com>
2025-07-23 11:57:09 +0530

    Rearrange calculator buttons for better layout and usability.

--- script.js
+++ script.js
@@ -4,5 +4,13 @@ let buttons = document.querySelectorAll('button');
 let string = "";
 let arr =  Array.from(buttons);
 arr.forEach(button => {
-    button.addEventListener(' ')
+    button.addEventListener('click', (e) => {
+        if(e.target.innerHTML == '=') {
+            string = eval(string);
+            input.value = string;
+
+        }
+        string += e.target.innerHTML;
+        input.value = string;
+    })
 })
